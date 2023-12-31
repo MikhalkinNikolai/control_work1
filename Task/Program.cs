3a1d231820acb4b2493511f3a4dddc7d91a351ed
MikhalkinNikolai <gfer.gfer@mail.ru>
2023-12-31 18:08:12 +0300

    Написал программу решения задачи

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] SecondArray(string[] array){
+    string[] array1 = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (array[i].Length <= 3){
+            array1[count] = array[i];
+            count++;
+        }
+    }
+    return array1;
+}
+
+void PrintArray(string[] array){
+    for (int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]}" + " ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = ["Hello","2","world",":-)","--"];
+string[] secondArray = SecondArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(secondArray);
